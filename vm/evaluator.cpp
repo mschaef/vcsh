mschaef/vcsh

commit ee00bf7f29cf8488e667ea02c986fcdde3e3eae9

Removed the C implementation of prog1. Reorganized symbol evaluation slightly. Currently, the compiler leaves a number of special forms as symbols in the function place, which means symbol evaluation can't assume a local symbol.

vm/evaluator.cpp
@@ -846,27 +846,15 @@ namespace scan {
           }
         else if (type == TC_SYMBOL)
           {
-            LRef local_binding;
-            LRef global_binding;
+            LRef local_binding = lenvlookup(form, env);
 
-            local_binding = lenvlookup(form, env);
-
-            if (!NULLP(local_binding))
-              {
-                retval = CAR(local_binding);
-
-                if (UNBOUND_MARKER_P(retval))
-                  vmerror_unbound(form);
-              }
+            if(NULLP(local_binding))
+              retval = SYMBOL_VCELL(form);
             else
-              {
-                global_binding = SYMBOL_VCELL(form);
+              retval = CAR(local_binding);
 
-                if (UNBOUND_MARKER_P(global_binding))
-                  vmerror_unbound (form);
-
-                retval = global_binding;
-              }
+            if (UNBOUND_MARKER_P(retval))
+              vmerror_unbound(form);
           }
         else if (type == TC_CONS)
           {
@@ -1276,24 +1264,6 @@ namespace scan {
     return new_closure;
   }
 
-  LRef lprogn (LRef * pform, LRef * penv)
-  {
-    LRef env, l, next;
-
-    env = *penv;
-    l = lcdr(*pform);
-    next = lcdr(l);
-    while (!NULLP (next))
-      {
-        leval(lcar(l), env);
-        l = next;
-        next = lcdr(next);
-      }
-    *pform = lcar(l);
-    return boolcons(true);
-  }
-
-
   LRef lor (LRef * pform, LRef * penv)
   {
     LRef val;
@@ -1422,20 +1392,17 @@ namespace scan {
     return (NIL);
   }
 
-
-  LRef lprog1 (LRef args, LRef env)
+  LRef lprogn (LRef args, LRef env)
   {
-    LRef retval,l;
+    LRef retval = NIL;
 
-    retval = leval(lcar(args), env);
-
-    for (l = lcdr(args); !NULLP(l); l = lcdr(l))
-      leval(lcar(l), env);
+    for (LRef l = args; !NULLP(l); l = lcdr(l))
+      retval = leval(lcar(l), env);
 
     return retval;
   }
 
-  LRef ltime (LRef args, LRef env)
+  LRef ltime (LRef args, LRef form)
   {
     fixnum_t cells      = interp.gc_total_cells_allocated;
     fixnum_t env_cells  = interp.gc_total_environment_cells_allocated;
@@ -1445,10 +1412,8 @@ namespace scan {
     flonum_t gc_t       = interp.gc_total_run_time;
     size_t forms        = interp.forms_evaluated;
 
-    LRef rc = lprog1(args, env);
-
     LRef argv[8];
-    argv[0] = rc;
+    argv[0] = lprogn(args, form);
     argv[1] = flocons(sys_runtime() - t);
     argv[2] = flocons(interp.gc_total_run_time - gc_t);
     argv[3] = fixcons(interp.gc_total_cells_allocated - cells);

vm/main.cpp
@@ -581,7 +581,7 @@ namespace scan {
     register_subr(_T("assv"),                             SUBR_2,     (void*)lassv                               );
     register_subr(_T("atan"),                             SUBR_2,     (void*)latan                               );
     register_subr(_T("add-symbol-to-package"),            SUBR_2,     (void*)ladd_symbol_to_package              );
-    register_subr(_T("begin"),                            SUBR_MACRO, (void*)lprogn                              );
+    register_subr(_T("begin"),                            SUBR_F,     (void*)lprogn                              );
     register_subr(_T("binary-port?"),                     SUBR_1,     (void*)lbinary_portp                       );
     register_subr(_T("bitwise-and"),                      SUBR_2N,    (void*)lbitwise_and                        );
     register_subr(_T("bitwise-arithmatic-shift-right"),   SUBR_2,     (void*)lbitwise_ashr                       );
@@ -721,7 +721,6 @@ namespace scan {
     register_subr(_T("primitive?"),                       SUBR_1,     (void*)lprimitivep                         );
     register_subr(_T("print-external-details"),           SUBR_2,     (void*)lprint_external_details             );
     register_subr(_T("procedure?"),                       SUBR_1,     (void*)lprocedurep                         );
-    register_subr(_T("prog1"),                            SUBR_F,     (void*)lprog1                              );
     register_subr(_T("quote"),                            SUBR_F,     (void*)lquote                              );
     register_subr(_T("quotient"),                         SUBR_2,     (void*)lquotient                           );
     register_subr(_T("random"),                           SUBR_1,     (void*)lrandom                             );

vm/scan.h
@@ -1857,7 +1857,7 @@ namespace scan {
   LRef lextend_env (LRef * pform, LRef * penv);
   LRef lcond (LRef * pform, LRef * penv);
   LRef lcase (LRef * pform, LRef * penv);
-  LRef lprogn (LRef * pform, LRef * penv);
+  LRef lprogn (LRef args, LRef env);
   LRef lset_handler_frames(LRef new_frames);
   LRef lhandler_frames();
   LRef lcatch (LRef args, LRef env);
@@ -1869,7 +1869,6 @@ namespace scan {
   LRef lsetq (LRef args, LRef env);
   LRef lquote (LRef args, LRef env);
   LRef lthe_environment (LRef args, LRef env);
-  LRef lprog1 (LRef args, LRef env);
   LRef ltime (LRef args, LRef env);
   LRef lpanic(LRef msg);
   LRef lexternal_data(LRef x);

scheme-core/exports.scm
@@ -637,7 +637,6 @@
            procedure-lambda-list
            procedure-name
            procedure?
-           prog1
            properties
            provide-package!
            push!

scheme-core/subrs.scm
@@ -228,7 +228,6 @@
 (define primitive? #.(scheme::%subr-by-name "primitive?"))
 (define print-external-details #.(scheme::%subr-by-name "print-external-details"))
 (define procedure? #.(scheme::%subr-by-name "procedure?"))
-(define prog1 #.(scheme::%subr-by-name "prog1"))
 (define quote #.(scheme::%subr-by-name "quote"))
 (define quotient #.(scheme::%subr-by-name "quotient"))
 (define random #.(scheme::%subr-by-name "random"))

commit fb8532cfd36f968eb7011f49c24d9824f3ba79f4

Restore TCO for begin forms. Fixes error introduced in ee00bf7f29cf8488e667ea02c986fcdde3e3eae9.

scc0/scc0
@@ -7,7 +7,4 @@ SCC0_BASE=`dirname $0`
 # scansh0 command line parser loads them.
 SCANPATH=${SCC0_BASE}/../scheme:${SCANPATH}
 
-${SCC0_BASE}/../vm/scansh0 ${SCC0_BASE}/scheme.scf \
-                           ${SCC0_BASE}/fasl-compiler-run.scf \
-                           ${SCC0_BASE}/fasl-compiler0.scf \
-                           $@
+${SCC0_BASE}/../vm/scansh0 ${SCC0_BASE}/scheme.scf ${SCC0_BASE}/fasl-compiler-run.scf ${SCC0_BASE}/fasl-compiler0.scf $@

vm/evaluator.cpp
@@ -1392,17 +1392,24 @@ namespace scan {
     return (NIL);
   }
 
-  LRef lprogn (LRef args, LRef env)
+  LRef lprogn (LRef * pform, LRef * penv)
   {
-    LRef retval = NIL;
+    LRef env, l, next;
 
-    for (LRef l = args; !NULLP(l); l = lcdr(l))
-      retval = leval(lcar(l), env);
-
-    return retval;
+    env = *penv;
+    l = lcdr(*pform);
+    next = lcdr(l);
+    while (!NULLP (next))
+      {
+        leval(lcar(l), env);
+        l = next;
+        next = lcdr(next);
+      }
+    *pform = lcar(l);
+    return boolcons(true);
   }
 
-  LRef ltime (LRef args, LRef form)
+  LRef ltime (LRef args, LRef env)
   {
     fixnum_t cells      = interp.gc_total_cells_allocated;
     fixnum_t env_cells  = interp.gc_total_environment_cells_allocated;
@@ -1412,8 +1419,14 @@ namespace scan {
     flonum_t gc_t       = interp.gc_total_run_time;
     size_t forms        = interp.forms_evaluated;
 
+
+    LRef retval = NIL;
+    for (LRef l = args; !NULLP(l); l = lcdr(l))
+      retval = leval(lcar(l), env);
+
     LRef argv[8];
-    argv[0] = lprogn(args, form);
+
+    argv[0] = retval;
     argv[1] = flocons(sys_runtime() - t);
     argv[2] = flocons(interp.gc_total_run_time - gc_t);
     argv[3] = fixcons(interp.gc_total_cells_allocated - cells);

vm/main.cpp
@@ -581,7 +581,7 @@ namespace scan {
     register_subr(_T("assv"),                             SUBR_2,     (void*)lassv                               );
     register_subr(_T("atan"),                             SUBR_2,     (void*)latan                               );
     register_subr(_T("add-symbol-to-package"),            SUBR_2,     (void*)ladd_symbol_to_package              );
-    register_subr(_T("begin"),                            SUBR_F,     (void*)lprogn                              );
+    register_subr(_T("begin"),                            SUBR_MACRO, (void*)lprogn                              );
     register_subr(_T("binary-port?"),                     SUBR_1,     (void*)lbinary_portp                       );
     register_subr(_T("bitwise-and"),                      SUBR_2N,    (void*)lbitwise_and                        );
     register_subr(_T("bitwise-arithmatic-shift-right"),   SUBR_2,     (void*)lbitwise_ashr                       );

vm/scan.h
@@ -1857,7 +1857,7 @@ namespace scan {
   LRef lextend_env (LRef * pform, LRef * penv);
   LRef lcond (LRef * pform, LRef * penv);
   LRef lcase (LRef * pform, LRef * penv);
-  LRef lprogn (LRef args, LRef env);
+  LRef lprogn (LRef * pform, LRef * penv);
   LRef lset_handler_frames(LRef new_frames);
   LRef lhandler_frames();
   LRef lcatch (LRef args, LRef env);