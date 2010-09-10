fc4331846b11e53c0bf2f35ac61eaaec7a51f9db
mschaef <schaeffer.michael.a@gmail.com>
2010-09-10 14:23:58 -0400

    Indent scheme-core.

--- scheme-core/scanlib.cpp
+++ scheme-core/scanlib.cpp
@@ -1,3 +1,4 @@
+
 /* scanlib.cpp
  * January 16th, 2006
  * 
@@ -7,37 +8,37 @@
 
 #include "../vm/scan.h"
 
-extern unsigned char scmFaslCompilerRun[]; // REVISIT: need to change this to _TCHAR
+extern unsigned char scmFaslCompilerRun[];      // REVISIT: need to change this to _TCHAR
 extern unsigned int scmFaslCompilerRun_bytes;
 
-namespace scan {
+BEGIN_NAMESPACE(scan)
+void scanlib_register_internal_files();
 
-  void scanlib_register_internal_files();
+void init1()
+{
+     /* Load and evaluate the .scm initialization code */
 
-  void init1()
-  {
-    /* Load and evaluate the .scm initialization code */
+     register_internal_file(_T("fasl-compiler-run"), true,
+                            scmFaslCompilerRun, scmFaslCompilerRun_bytes);
 
-    register_internal_file(_T("fasl-compiler-run"), true, 
-                           scmFaslCompilerRun, scmFaslCompilerRun_bytes);
+     register_internal_file(_T("s-core"), true, scmSCore, scmSCore_bytes);
 
-    register_internal_file(_T("s-core"), true, scmSCore, scmSCore_bytes);
+     ENTER_TRY(NIL)
+     {
+          lifasl_load(open_c_data_input(true, scmSCore, scmSCore_bytes));
+     } ON_ERROR()
+     {
+          assert(SYMBOLP(interp.sym_errobj));
 
-    ENTER_TRY(NIL) {
-      lifasl_load(open_c_data_input(true, scmSCore, scmSCore_bytes));
-    } ON_ERROR() {
-      assert(SYMBOLP(interp.sym_errobj));
-
-      scwritef("\nError loading sinit, errobj = ~a\n", 
-	       DEFAULT_PORT, 
-	       SYMBOL_VCELL(interp.sym_errobj));
-    } LEAVE_TRY();
-  }
-
-  void init(int argc, _TCHAR *argv[], debug_flag_t initial_debug_flags)
-  {
-    init0(argc, argv, initial_debug_flags);
-    init1();
-  }
-
+          scwritef("\nError loading sinit, errobj = ~a\n",
+                   DEFAULT_PORT, SYMBOL_VCELL(interp.sym_errobj));
+     } LEAVE_TRY();
 }
+
+void init(int argc, _TCHAR * argv[], debug_flag_t initial_debug_flags)
+{
+     init0(argc, argv, initial_debug_flags);
+     init1();
+}
+
+END_NAMESPACE
--- scheme-core/vcsh.cpp
+++ scheme-core/vcsh.cpp
@@ -1,45 +1,38 @@
+
 /* vcsh.cpp
  *
  * The vcalc console's main 'shell' program.
  */
 
-
-
-
-
-
 #include "../vm/scan.h"
 
 void sigint_handler(int i)
 {
-  UNREFERENCED(i);
+     UNREFERENCED(i);
 
-  scan::signal_break();
+     scan::signal_break();
 
-  signal(SIGINT, sigint_handler);
+     signal(SIGINT, sigint_handler);
 };
 
-using namespace scan;
-
-int _tmain(int argc, _TCHAR *argv[])
+int _tmain(int argc, _TCHAR * argv[])
 {
-  scan::sys_init();
-  scan::init(argc, argv, scan::DF_NONE);
+     scan::sys_init();
+     scan::init(argc, argv, scan::DF_NONE);
 
-  signal(SIGINT, sigint_handler);
+     signal(SIGINT, sigint_handler);
 
-  scan::LRef retval = scan::run();
+     scan::LRef retval = scan::run();
 
-  long return_status = 0;
+     long return_status = 0;
 
-  if (scan::NUMBERP(retval))
-    return_status = scan::get_c_long(retval);
+     if (scan::NUMBERP(retval))
+          return_status = scan::get_c_long(retval);
 
-  if (DEBUGGING_BUILD)
-    scwritef(_T(";;;; end run, rc=~cd\n"), CURRENT_DEBUG_PORT, return_status);
+     if (DEBUGGING_BUILD)
+          scwritef(_T(";;;; end run, rc=~cd\n"), scan::CURRENT_DEBUG_PORT(), return_status);
 
-  scan::shutdown();
+     scan::shutdown();
 
-  exit((int)return_status);
+     exit((int) return_status);
 }
-
