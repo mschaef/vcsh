d9684d34a92fd561238ff17144d3f1bb01303895
mschaef <schaeffer.michael.a@gmail.com>
2010-09-10 19:02:25 -0400

    Move external pointers into their own file.

--- vm/main.cpp
+++ vm/main.cpp
@@ -32,66 +32,6 @@ LRef lnotp(LRef x)
      return boolcons(!TRUEP(x));
 }
 
-/*** C-pointers ***/
-LRef externalcons(void *data, LRef desc, external_meta_t * meta /* = NULL */ )
-{
-     LRef z = new_cell(TC_EXTERNAL);
-
-     assert((meta == NULL) || (meta->_name != NULL));
-
-     SET_EXTERNAL_DATA(z, data);
-     SET_EXTERNAL_DESC(z, desc);
-     SET_EXTERNAL_META(z, meta);
-
-     return z;
-}
-
-LRef lexternal_data(LRef x)
-{
-     if (!EXTERNALP(x))
-          vmerror_wrong_type(1, x);
-
-     return fixcons((uptr_t) EXTERNAL_DATA(x));
-}
-
-LRef lexternal_desc(LRef x)
-{
-     if (!EXTERNALP(x))
-          vmerror_wrong_type(1, x);
-
-     return EXTERNAL_DESC(x);
-}
-
-LRef lexternalp(LRef x)
-{
-     if (EXTERNALP(x))
-          return x;
-     else
-          return boolcons(false);
-}
-
-LRef lexternal_type_name(LRef obj)
-{
-     if (!EXTERNALP(obj))
-          return boolcons(false);
-
-     if (EXTERNAL_META(obj))
-          return strcons(_T(EXTERNAL_META(obj)->_name));
-
-     return boolcons(true);
-}
-
-LRef lprint_external_details(LRef obj, LRef port)
-{
-     if (!EXTERNALP(obj))
-          vmerror_wrong_type(1, obj);
-
-     if (EXTERNAL_META(obj) && (EXTERNAL_META(obj)->_print_details))
-          return EXTERNAL_META(obj)->_print_details(obj, port);
-
-     return boolcons(false);
-}
-
 /**************************************************************
  * Equality tests
  */
