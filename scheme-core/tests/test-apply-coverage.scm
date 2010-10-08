1968c9fa29090bdc800350f5be22a9228d2b98d2
mschaef <schaeffer.michael.a@gmail.com>
2010-10-08 15:23:10 -0400

    Fix the test case failure.

--- scheme-core/tests/test-apply-coverage.scm
+++ scheme-core/tests/test-apply-coverage.scm
@@ -26,8 +26,9 @@
   (test-case (equal? '(1 2 3) (apply identity-rest 1 2 3 '())))
 
   (test-case (runtime-error? (apply null-result 2)))
-  (test-case (runtime-error? (apply identity-rest '(1 2 3 4 5 6 7 8
-                                                    9 10 11 12 13 14 15 16
-                                                    17 18 19 20 21 22 23 24 25)))))
+  (test-case (runtime-error? (apply identity-rest '(1 2 3 4 5 6 7 8 9 10 11 12
+                                                    13 14 15 16 17 18 19 20 21
+                                                    22 23 24 25 26 27 28 29 30
+                                                    31 32 33)))))
 
   
