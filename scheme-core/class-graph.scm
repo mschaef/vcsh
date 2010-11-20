eebc39601302e9635c83d6a5092c0cc12b604da9
mschaef <schaeffer.michael.a@gmail.com>
2010-11-20 12:27:47 -0500

    Add TC_GENV support to type-of.

--- scheme-core/class-graph.scm
+++ scheme-core/class-graph.scm
@@ -29,6 +29,7 @@
     ((#.system::TC_UNBOUND_MARKER) 'unbound-marker) 
     ((#.system::TC_GC_TRIP_WIRE)   'trip-wire) 
     ((#.system::TC_FAST_OP)        'fast-op)
+    ((#.system::TC_GENV)           'genv)
     (#t (error "Bad typecode: ~s." tc))))
 
 (define (%representation-of obj)
