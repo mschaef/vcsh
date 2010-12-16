431389c65440a30c3f98b54918979743c7504a5f
mschaef <schaeffer.michael.a@gmail.com>
2010-12-16 18:19:41 -0500

    Add a macro that grabs a foplog for a given form.

--- scheme-core/foplog.scm
+++ scheme-core/foplog.scm
@@ -2,7 +2,8 @@
 (define-package "foplog"
   (:uses "scheme")
   (:exports "fop-histogram"
-            "fop-pair-histogram"))
+            "fop-seq-histogram"
+            "foplog-for"))
 
 (define %foplog-reset #.(scheme::%subr-by-name "%foplog-reset" #L0(error "foplog support not found")))
 (define %foplog-enable #.(scheme::%subr-by-name "%foplog-enable" #L0(error "foplog support not found")))
@@ -12,6 +13,9 @@
   (if (not (null? op))
       (mvbind (opcode args) (scheme::parse-fast-op op) opcode)
       #f))
+
+(define (foplog->opcodes log)
+  (map opcode->name (vector->list log)))
 
 (define (foplog)
   (map opcode->name (vector->list (%foplog-snapshot))))
@@ -25,9 +29,18 @@
       (hash-set! counts entry (+ 1 (hash-ref counts entry 0))))
     (qsort (hash->a-list counts) > cdr)))
 
-(define (fop-histogram :optional (log (foplog)))
-  (hist log))
+(define (fop-histogram :optional (log (%foplog-snapshot)))
+  (hist (foplog->opcodes log)))
 
-(define (fop-seq-histogram :optional (length 2) (log (foplog)))
- (hist (foplog-seqs log length)))
+(define (fop-seq-histogram :optional (length 2) (log (%foplog-snapshot)))
+  (hist (foplog-seqs (foplog->opcodes log) length)))
 
+(defmacro (foplog-for expr)
+  `(begin
+     (when (%foplog-enable #f)
+       (error "foplog already enabled."))
+     (%foplog-reset)
+     (%foplog-enable #t)
+     ,expr
+     (%foplog-enable #f)
+     (%foplog-snapshot)))
