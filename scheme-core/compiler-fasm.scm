05bcd75fe746d2d57037ef240ae0b343ba94358b
mschaef <schaeffer.michael.a@gmail.com>
2011-01-07 13:12:36 -0500

    Add FOP metadata indicating FOP argument types.

--- scheme-core/compiler-fasm.scm
+++ scheme-core/compiler-fasm.scm
@@ -7,56 +7,47 @@
 (define-structure fast-op-definition
   opcode
   name
-  arity)
+  arity
+  args)
 
 (define *fast-op-names* (make-hash :eq))
 (define *fast-op-opcodes* (make-hash :eq))
 
-(define (extend-fast-op-names! op-name op-code arity)
+(define (extend-fast-op-names! op-name op-code arity args)
   (when (hash-has? *fast-op-names* op-name)
     (error "Fast op already defined: ~s" op-name))
   (when (hash-has? *fast-op-opcodes* op-code)
     (error "Fast opcode for ~s already defined: ~s" op-name op-code))
   (let ((defn (make-fast-op-definition :name op-name
-                            :opcode op-code
-                            :arity arity)))
+                                       :opcode op-code
+                                       :arity arity
+                                       :args args)))
     (hash-set! *fast-op-names* op-name defn)
     (hash-set! *fast-op-opcodes* op-code defn)))
 
-(defmacro (define-fast-op op-name op-code arity)
+(defmacro (define-fast-op op-name op-code . args)
   `(eval-when (:load-toplevel :compile-toplevel :execute)
-     (extend-fast-op-names! ,op-name ,op-code ,arity)))
+     (extend-fast-op-names! ,op-name ,op-code ,(length args) ',args)))
 
-
-(define-fast-op :literal #.system::FOP_LITERAL 1)
-(define-fast-op :global-ref #.system::FOP_GLOBAL_REF 1)
-(define-fast-op :global-set! #.system::FOP_GLOBAL_SET 2)
-(define-fast-op :local-ref #.system::FOP_LOCAL_REF 1)
-(define-fast-op :local-set! #.system::FOP_LOCAL_SET 2)
- 
-(define-fast-op :apply #.system::FOP_APPLY 2)
- 
-(define-fast-op :if-true #.system::FOP_IF_TRUE 3)
- 
-(define-fast-op :and/2 #.system::FOP_AND2 2)
-(define-fast-op :or/2 #.system::FOP_OR2 2)
- 
-(define-fast-op :sequence #.system::FOP_SEQUENCE 2)
- 
-(define-fast-op :throw #.system::FOP_THROW 2)
- 
-(define-fast-op :catch #.system::FOP_CATCH 2)
-(define-fast-op :with-unwind-fn #.system::FOP_WITH_UNWIND_FN 2)
- 
-(define-fast-op :close-env #.system::FOP_CLOSE_ENV 2)
- 
-(define-fast-op :get-env #.system::FOP_GET_ENV 0)
- 
-(define-fast-op :global-def #.system::FOP_GLOBAL_DEF 3)
- 
-(define-fast-op :get-fsp #.system::FOP_GET_FSP 0)
-(define-fast-op :get-hframes #.system::FOP_GET_HFRAMES 0)
-(define-fast-op :set-hframes #.system::FOP_SET_HFRAMES 1)
+(define-fast-op :literal        #.system::FOP_LITERAL        :literal                  )
+(define-fast-op :global-ref     #.system::FOP_GLOBAL_REF     :symbol                   )
+(define-fast-op :global-set!    #.system::FOP_GLOBAL_SET     :symbol :fast-op          )
+(define-fast-op :local-ref      #.system::FOP_LOCAL_REF      :symbol                   )
+(define-fast-op :local-set!     #.system::FOP_LOCAL_SET      :symbol :fast-op          )
+(define-fast-op :apply          #.system::FOP_APPLY          :symbol (:fast-op)        )
+(define-fast-op :if-true        #.system::FOP_IF_TRUE        :fast-op :fast-op :fast-op)
+(define-fast-op :and/2          #.system::FOP_AND2           :fast-op :fast-op         )
+(define-fast-op :or/2           #.system::FOP_OR2            :fast-op :fast-op         )
+(define-fast-op :sequence       #.system::FOP_SEQUENCE       :fast-op :fast-op         )
+(define-fast-op :throw          #.system::FOP_THROW          :fast-op :fast-op         )
+(define-fast-op :catch          #.system::FOP_CATCH          :fast-op :fast-op         )
+(define-fast-op :with-unwind-fn #.system::FOP_WITH_UNWIND_FN :fast-op :fast-op         )
+(define-fast-op :close-env      #.system::FOP_CLOSE_ENV      :fast-op :literal         )
+(define-fast-op :get-env        #.system::FOP_GET_ENV                                  )
+(define-fast-op :global-def     #.system::FOP_GLOBAL_DEF     :symbol :literal          )
+(define-fast-op :get-fsp        #.system::FOP_GET_FSP                                  )
+(define-fast-op :get-hframes    #.system::FOP_GET_HFRAMES                              )
+(define-fast-op :set-hframes    #.system::FOP_SET_HFRAMES    :fast-op                  )
 
 (define (parse-fast-op fast-op :optional (parse-opcode? #t))
   (let ((opcode (scheme::%fast-op-opcode fast-op))
