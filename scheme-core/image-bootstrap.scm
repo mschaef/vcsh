fdc03c85aefc78f3750c24388a0776c4610d2b12
mschaef <schaeffer.michael.a@gmail.com>
2011-01-22 16:00:59 -0500

    Rename boot- to early-

--- scheme-core/image-bootstrap.scm
+++ scheme-core/image-bootstrap.scm
@@ -4,7 +4,7 @@
 ;;; immediately after the constants and subrs are loaded. They're used
 ;;; to make it easier to boot up an otherwise empty image.
 ;;;
-;;; Code in here has to follow a few rules:
+;;; Code in herxe has to follow a few rules:
 ;;; 1) The only definitions directly availble are subrs and constants.
 ;;;
 ;;; 2) It's okay to use host-scheme:: to get at the host scheme environment,
@@ -14,9 +14,11 @@
 
 (host-scheme::format (host-scheme::current-error-port) "; Loading bootstrap definitions\n")
 
+(host-scheme::in-package! "scheme")
+
 (host-scheme::define (list . x) x)
 
-(host-scheme::defmacro (%boot-dbind binding value . code)
+(host-scheme::defmacro (%early-dbind binding value . code)
    (host-scheme::define (binding-forms binding value)
        (host-scheme::cond ((symbol? binding)
                            (list (list binding value)))
@@ -33,29 +35,32 @@
           ,@code))))
 
 
-(host-compiler::defmacro (%boot-defmacro lambda-list . macro-body) ;; TODO: Macros with :optional arguments
+(host-compiler::defmacro (%early-defmacro lambda-list . macro-body) ;; TODO: Macros with :optional arguments
   (host-scheme::let ((macro-body macro-body) ; shadowed the argument to give us something settable.
                      (macro-name (car lambda-list))
                      (macro-formals (cdr lambda-list)))
     `(%define ,macro-name
               (%macro (%lambda () (form env)
-                               (%boot-dbind ,macro-formals (cdr form)
+                               (%early-dbind ,macro-formals (cdr form)
                                  ,@macro-body))))))
 
-(%boot-defmacro (%boot-named-lambda name l-list . code)
+(%early-defmacro (%early-named-lambda name l-list . code)
   (if name
       `(%lambda ((scheme::name . ,name)) ,l-list ,@code)
       `(%lambda () ,l-list ,@code)))
 
-(%boot-defmacro (%boot-lambda arglist . code)
-  `(%boot-named-lambda #f ,arglist ,@code))
+(%early-defmacro (%early-lambda arglist . code)
+  `(%early-named-lambda #f ,arglist ,@code))
 
-(%boot-defmacro (%boot-define name . defn)
+(%early-defmacro (%early-define name . defn)
   (if (symbol? name)
       `(%define ,name ,(car defn))
       (if (and (pair? name) (symbol? (car name)))
-          `(%define ,(car name)   (%boot-named-lambda ,(car name) ,(cdr name) ,@defn))
-          (host-scheme::error "Invalid syntax for %boot-define: ~a" (cons name defn)))))
+          `(%define ,(car name)   (%early-named-lambda ,(car name) ,(cdr name) ,@defn))
+          (host-scheme::error "Invalid syntax for %early-define: ~a" (cons name defn)))))
 
+(%early-defmacro (%early-let variables . forms)
+  (host-scheme::mvbind (variable-names variable-binding-forms) (host-scheme::parse-let-variables variables)
+    `((%early-lambda ,variable-names ,@forms) ,@variable-binding-forms)))
 
 (host-scheme::format (host-scheme::current-error-port) "; Done loading bootstrap definitions\n")
