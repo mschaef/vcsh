30facf5788ef130585fa4049d23e797570aec691
mschaef <schaeffer.michael.a@gmail.com>
2011-02-11 16:31:32 -0500

    Add constants.h.

---
+++ vm/constants.h
@@ -0,0 +1,13 @@
+
+#ifndef __CONSTANTS_H
+#define __CONSTANTS_H
+
+BEGIN_NAMESPACE(scan)
+
+#define CONST_C_HEADER
+#include "constants.i"
+#undef CONST_C_HEADER
+
+END_NAMESPACE;
+
+#endif __CONSTANTS_H
