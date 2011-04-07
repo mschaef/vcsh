16e5e979304f540a9878bf77570b3d8613ae57ae
mschaef <schaeffer.michael.a@gmail.com>
2011-04-06 20:32:15 -0400

    Move the (very old and unused) signature-list->english into signature-list.scm.

--- scheme-core/generic-functions.scm
+++ scheme-core/generic-functions.scm
@@ -12,31 +12,6 @@
 
 ;;; Generic Functions
 
-(define (signature-list->english type-names)
-  "Given a list of symbols, <type-names>, return an English string
-   listing each element."
-  (define (vowel? letter)
-    (member letter '(#\a #\e #\i #\o #\u #\y #\A #\E #\I #\O #\U #\Y)))
-  (define (type-name-connective type-name)
-    (cond ((eq? type-name #t) "a")
-          ((vowel? (string-ref (symbol-name type-name) 0)) "an")
-          (#t "a")))  (let ((need-commas? (> (length type-names) 2))
-        (need-and? (> (length type-names) 1))
-        (op (open-output-string)))
-    (let loop ((names type-names) (pos 0))
-      (cond ((null? names) (get-output-string op))
-            (#t
-             (unless (or (symbol? (car names)) (eq? (car names) #t))
-               (error "Type names must be symbols or #t: ~a" (car names)))
-             (when (and (> pos 0) need-commas?) (display ", " op))
-
-             (when (null? (cdr names))
-               (when need-and?
-                 (when (= pos 1) (display " " op))
-                 (display "and " op)))
-             (format op "~a ~a" (type-name-connective (car names)) (car names))
-             (loop (cdr names) (+ 1 pos)))))))
-
 (define (generic-function? obj)
   "Returns #t if <obj> is a generic function."
   (and (closure? obj)
