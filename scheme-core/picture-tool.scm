324bed3229b7da76046d261ccdd1cce01e9f7aef
mschaef <schaeffer.michael.a@gmail.com>
2011-05-16 13:00:04 -0400

    Refactor picture-tool.scm

--- scheme-core/picture-tool.scm
+++ scheme-core/picture-tool.scm
@@ -3,31 +3,30 @@
 
 (define *picture-target* "~/picture-target")
 
-(define *dry-run* #f)
+(define *dry-run* #t)
 
 (define (system-command cmd)
   (format (current-error-port) "; system: ~s\n" cmd)
   (unless *dry-run*
     (system cmd)))
 
-(define (sha1-digests filenames)
+(define (find-import-details filenames)
   (map (lambda (filename)
-          (list filename
-                (file-sha1-digest filename)))
+         (let ((details (alist 'source-filename filename)))
+           (if (file-exists? filename)
+               (let ((source-digest (file-sha1-digest filename)))
+                 (alist 'source-digest source-digest
+                        'target-filename (make-filename *picture-target*
+                                                        (string-append source-digest "." (filename-extension filename)))
+                        details))
+               details)))
        filenames))
 
-(define (summarize-import filenames)
-  (map (lambda (filename/digest)
-         (dbind (filename digest) filename/digest
-           (list filename
-                 (make-filename *picture-target*
-                                (string-append digest "." (filename-extension filename))))))
-       (sha1-digests filenames)))
-
 (define (copy-file from to)
-  (system-command #"cp ${from} ${to}"))
+  (system-command #"cp ${from} ${to}")) ;; TODO: does this syntax handle explicit packages correctly? What about symbols in packages other than *package*?
 
 (define (do-import filenames)
-  (dolist (s/t (summarize-import filenames))
-    (dbind (source target) s/t
-      (copy-file source target))))
+  (dolist (import-info (find-import-details filenames))
+    (when (assq 'target-filename import-info)
+      (copy-file (cdr (assq 'source-filename import-info ))
+                 (cdr (assq 'target-filename import-info ))))))
