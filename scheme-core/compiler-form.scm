2e686b58883c1f1f116e2b216fa77657ab6debb6
mschaef <schaeffer.michael.a@gmail.com>
2011-08-15 21:17:12 -0400

    Simplify the form compiler internal protocols. Update scc0.

--- scheme-core/compiler-form.scm
+++ scheme-core/compiler-form.scm
@@ -10,18 +10,16 @@
 ;;;; redistribution of this file, and for a DISCLAIMER OF ALL
 ;;;; WARRANTIES.
 
-(define (form->fop-assembly form :optional (at-toplevel? #f))
-  (expanded-form-meaning
-   (expand-form form at-toplevel?) ()))
-
-(define (compile-form form :optional (at-toplevel? #f))
+(define (compile-form form)
   "Accept a <form> and compile it into the corresponding closure or literal. <form>
    must be a form that either explicitly defines a closure or a literal."
   (fop-assemble
    (optimize-fop-assembly
-    (form->fop-assembly form at-toplevel?))))
+    (expanded-form-meaning
+     (expand-form form #t)
+     ()))))
 
 (define (toplevel-form->thunk form)
   "Accept a <form> and compile it into a closure that can be invoked to produce the
   effect of evaluating that form at toplevel."
-  (compile-form `(compiler::%toplevel-lambda ,form) #t))
+  (compile-form `(compiler::%toplevel-lambda ,form)))
