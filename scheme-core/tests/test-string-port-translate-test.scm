ea66237f3f3fab9827a8dbd7c47ea2a2c3a1ed28
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-01-07 15:00:32 -0500

    Correct string port translate mode tests.

--- scheme-core/tests/test-string-port-translate-test.scm
+++ scheme-core/tests/test-string-port-translate-test.scm
@@ -2,62 +2,59 @@
 
 (defmacro (output-string port-var . code)
   `(let ((,port-var (open-output-string)))
-     (set-port-translate-mode! ,port-var #t)
-      (begin ,@code)
-      (flush-port ,port-var)
-      (get-output-string ,port-var)))
+     ,@code
+     (get-output-string ,port-var)))
 
 (define-test string-port-translate-test
   (test-case (equal? (output-string p 
-                                (display "hello" p))
-                 "hello"))
+                                    (display "hello" p))
+                     "hello"))
   (test-case (equal? (output-string p 
-                                (display "hello\r" p))
-                 "hello\r\n"))
+                                    (display "hello\r" p))
+                     "hello\r"))
   (test-case (equal? (output-string p 
-                                (display "hello\n" p))
-                 "hello\r\n"))
+                                    (display "hello\n" p))
+                     "hello\n"))
   (test-case (equal? (output-string p 
-                                (display "hello\r\n" p))
-                 "hello\r\n"))
+                                    (display "hello\r\n" p))
+                     "hello\r\n"))
   (test-case (equal? (output-string p 
-                                (display "\rhello" p))
-                 "\r\nhello"))
+                                    (display "\rhello" p))
+                     "\rhello"))
   (test-case (equal? (output-string p 
-                                (display "\nhello" p))
-                 "\r\nhello"))
+                                    (display "\nhello" p))
+                     "\nhello"))
   (test-case (equal? (output-string p 
-                                (display "\r\nhello" p))
-                 "\r\nhello"))
+                                    (display "\r\nhello" p))
+                     "\r\nhello"))
   (test-case (equal? (output-string p 
-                                (display "hello\rworld" p))
-                 "hello\r\nworld"))
+                                    (display "hello\rworld" p))
+                     "hello\rworld"))
   (test-case (equal? (output-string p 
-                                (display "hello\nworld" p))
-                 "hello\r\nworld"))
+                                    (display "hello\nworld" p))
+                     "hello\nworld"))
   (test-case (equal? (output-string p 
-                                (display "hello\r\nworld" p))
-                 "hello\r\nworld"))
+                                    (display "hello\r\nworld" p))
+                     "hello\r\nworld"))
   (test-case (equal? (output-string p 
-                                (display "hello" p)
-                                (display "world" p))
-                 "helloworld"))
+                                    (display "hello" p)
+                                    (display "world" p))
+                     "helloworld"))
   (test-case (equal? (output-string p 
-                                (display "hello\r" p)
-                                (display "world" p))
-                 "hello\r\nworld"))
+                                    (display "hello\r" p)
+                                    (display "world" p))
+                     "hello\rworld"))
   (test-case (equal? (output-string p 
-                                (display "hello\n" p)
-                                (display "world" p))
-                 "hello\r\nworld"))
+                                    (display "hello\n" p)
+                                    (display "world" p))
+                     "hello\nworld"))
   (test-case (equal? (output-string p 
-                                (display "hello\r\n" p)
-                                (display "world" p))
-                 "hello\r\nworld"))
+                                    (display "hello\r\n" p)
+                                    (display "world" p))
+                     "hello\r\nworld"))
   (test-case (equal? (output-string p 
-                                (display "hello\r" p)
-                                (display "\nworld" p))
-                 "hello\r\nworld")))
+                                    (display "hello\r" p)
+                                    (display "\nworld" p))
+                     "hello\r\nworld")))
 
 
-     
