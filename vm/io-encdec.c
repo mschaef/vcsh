bfecbbdaffacc42fc61bb943956b2930292a5ebc
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-03-26 12:19:23 -0400

    Remove some redundant code in io-encdec.c

--- vm/io-encdec.c
+++ vm/io-encdec.c
@@ -25,19 +25,19 @@ void io_encode_fixnum_s64(uint8_t *buf, fixnum_t num)
      buf[4] = (uint8_t)(num >> 24);
      buf[5] = (uint8_t)(num >> 16);
      buf[6] = (uint8_t)(num >>  8);
-     buf[7] = (uint8_t)(num >>  0);
+     buf[7] = (uint8_t)num;
 }
 
 fixnum_t io_decode_fixnum_s64(uint8_t *buf)
 {
-     return (((fixnum_t)(buf[0] & 255) << 56) +
-             ((fixnum_t)(buf[1] & 255) << 48) +
-             ((fixnum_t)(buf[2] & 255) << 40) +
-             ((fixnum_t)(buf[3] & 255) << 32) +
-             ((fixnum_t)(buf[4] & 255) << 24) +
-             ((fixnum_t)(buf[5] & 255) << 16) +
-             ((fixnum_t)(buf[6] & 255) <<  8) +
-             ((fixnum_t)(buf[7] & 255) <<  0));
+     return (((fixnum_t)buf[0] << 56) +
+             ((fixnum_t)buf[1] << 48) +
+             ((fixnum_t)buf[2] << 40) +
+             ((fixnum_t)buf[3] << 32) +
+             ((fixnum_t)buf[4] << 24) +
+             ((fixnum_t)buf[5] << 16) +
+             ((fixnum_t)buf[6] <<  8) +
+             ((fixnum_t)buf[7]));
 }
 
 /***** Unsigned 64 bit *****/
@@ -51,7 +51,7 @@ void io_encode_fixnum_u64(uint8_t *buf, unsigned_fixnum_t num)
      buf[4] = (uint8_t)(num >> 24);
      buf[5] = (uint8_t)(num >> 16);
      buf[6] = (uint8_t)(num >>  8);
-     buf[7] = (uint8_t)(num >>  0);
+     buf[7] = (uint8_t)num;
 }
 
 unsigned_fixnum_t io_decode_fixnum_u64(uint8_t *buf)
@@ -73,7 +73,7 @@ void io_encode_fixnum_s32(uint8_t *buf, fixnum_t num)
      buf[0] = (uint8_t)(num >> 24);
      buf[1] = (uint8_t)(num >> 16);
      buf[2] = (uint8_t)(num >>  8);
-     buf[3] = (uint8_t)(num >>  0);
+     buf[3] = (uint8_t)num;
 }
 
 fixnum_t io_decode_fixnum_s32(uint8_t *buf)
@@ -81,7 +81,7 @@ fixnum_t io_decode_fixnum_s32(uint8_t *buf)
      return (((int32_t)(buf[0] << 24)) +
              ((int32_t)(buf[1] << 16)) +
              ((int32_t)(buf[2] <<  8)) +
-             ((int32_t)(buf[3] <<  0)));
+             ((int32_t)buf[3]));
 }
 
 /***** Unsigned 32 bit *****/
@@ -91,7 +91,7 @@ void io_encode_fixnum_u32(uint8_t *buf, unsigned_fixnum_t num)
      buf[0] = (uint8_t)(num >> 24);
      buf[1] = (uint8_t)(num >> 16);
      buf[2] = (uint8_t)(num >>  8);
-     buf[3] = (uint8_t)(num >>  0);
+     buf[3] = (uint8_t)num;
 }
 
 unsigned_fixnum_t io_decode_fixnum_u32(uint8_t *buf)
@@ -99,20 +99,20 @@ unsigned_fixnum_t io_decode_fixnum_u32(uint8_t *buf)
      return (((uint32_t)buf[0] << 24) +
              ((uint32_t)buf[1] << 16) +
              ((uint32_t)buf[2] << 8) +
-             ((uint32_t)buf[3] << 0));
+             ((uint32_t)buf[3]));
 }
 /***** Signed 16 bit *****/
 
 void io_encode_fixnum_s16(uint8_t *buf, fixnum_t num)
 {
      buf[0] = (uint8_t)(num >>  8);
-     buf[1] = (uint8_t)(num >>  0);
+     buf[1] = (uint8_t)num;
 }
 
 fixnum_t io_decode_fixnum_s16(uint8_t *buf)
 {
      return ((int16_t)(buf[0] << 8) +
-             (int16_t)(buf[1] << 0));
+             (int16_t)buf[1]);
 }
 
 /***** Unsigned 16 bit *****/
@@ -120,37 +120,37 @@ fixnum_t io_decode_fixnum_s16(uint8_t *buf)
 void io_encode_fixnum_u16(uint8_t *buf, unsigned_fixnum_t num)
 {
      buf[0] = (uint8_t)(num >>  8);
-     buf[1] = (uint8_t)(num >>  0);
+     buf[1] = (uint8_t)num;
 }
 
 unsigned_fixnum_t io_decode_fixnum_u16(uint8_t *buf)
 {
      return (((uint16_t)buf[0] <<  8) +
-             ((uint16_t)buf[1] <<  0));
+             ((uint16_t)buf[1]));
 }
 
 /***** Signed 8 bit *****/
 
 void io_encode_fixnum_s8(uint8_t *buf, fixnum_t num)
 {
-     buf[0] = (uint8_t)(num >>  0);
+     buf[0] = (uint8_t)num;
 }
 
 fixnum_t io_decode_fixnum_s8(uint8_t *buf)
 {
-     return (int8_t)(buf[0]) <<  0;
+     return (int8_t)buf[0];
 }
 
 /***** Unsigned 8 bit *****/
 
 void io_encode_fixnum_u8(uint8_t *buf, unsigned_fixnum_t num)
 {
-     buf[0] = (uint8_t)(num >>  0);
+     buf[0] = (uint8_t)num;
 }
 
 unsigned_fixnum_t io_decode_fixnum_u8(uint8_t *buf)
 {
-     return (uint8_t)(buf[0] <<  0);
+     return (uint8_t)buf[0];
 }
 
 
