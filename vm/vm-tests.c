52cd86b7434b683c43eac33f9afc102d4c1dc36f
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-03-26 11:47:08 -0400

    Add unit tests for each type of encode decode scenario.

--- vm/vm-tests.c
+++ vm/vm-tests.c
@@ -15,7 +15,7 @@
 
 #include "scan-private.h"
 
-static size_t test_fail_count = 0;
+static size_t assert_fail_count = 0;
 static _TCHAR *test_name = NULL;
 
 static void invoke_test(void (* test_fn)(), _TCHAR *test_fn_name)
@@ -24,7 +24,7 @@ static void invoke_test(void (* test_fn)(), _TCHAR *test_fn_name)
 
      test_name = test_fn_name;
 
-     fprintf(stderr, "Executing VM Test: %s\n", test_name);
+     fprintf(stderr, "* %s\n", test_name);
 
      test_fn();
 
@@ -36,9 +36,9 @@ static bool test_assert(bool condition, _TCHAR *condition_name)
      if (condition)
           return false;
 
-     fprintf(stderr, "Failure in %s: %s\n", test_name, condition_name);
+     fprintf(stderr, "** FAIL (%s): %s\n", test_name, condition_name);
 
-     test_fail_count++;
+     assert_fail_count++;
 
      return true;
 }
@@ -47,31 +47,50 @@ static bool test_assert(bool condition, _TCHAR *condition_name)
 
 #define TEST_ASSERT(condition) test_assert(condition, _T(#condition))
 
-
-static void test_encdec_i8()
+static void test_encode_decode(void (* encode)(uint8_t *buf, fixnum_t num),
+                               fixnum_t (* decode)(uint8_t *buf))
 {
      fixnum_t ii;
      fixnum_t ii2;
      uint8_t buf[8];
 
      for(ii = -4; ii < 5; ii++) {
-          io_encode_fixnum_s16(buf, ii);
-          ii2 = io_decode_fixnum_s16(buf);
+          encode(buf, ii);
+          ii2 = decode(buf);
 
           if (TEST_ASSERT(ii == ii2))
-               fprintf(stderr, _T("%" PRINTF_PREFIX_FIXNUM "i == %" PRINTF_PREFIX_FIXNUM "i\n"), ii, ii2);
+               fprintf(stderr, _T("%" PRINTF_PREFIX_FIXNUM "i != %" PRINTF_PREFIX_FIXNUM "i\n"), ii, ii2);
      }
 }
 
+static void test_encdec_s8 () { test_encode_decode(io_encode_fixnum_s8 , io_decode_fixnum_s8);  }
+static void test_encdec_u8 () { test_encode_decode(io_encode_fixnum_u8 , io_decode_fixnum_u8);  }
+
+static void test_encdec_s16() { test_encode_decode(io_encode_fixnum_s16, io_decode_fixnum_s16); }
+static void test_encdec_u16() { test_encode_decode(io_encode_fixnum_u16, io_decode_fixnum_u16); }
+
+static void test_encdec_s32() { test_encode_decode(io_encode_fixnum_s32, io_decode_fixnum_s32); }
+static void test_encdec_u32() { test_encode_decode(io_encode_fixnum_u32, io_decode_fixnum_u32); }
+
+static void test_encdec_s64() { test_encode_decode(io_encode_fixnum_s64, io_decode_fixnum_s64); }
+static void test_encdec_u64() { test_encode_decode(io_encode_fixnum_u64, io_decode_fixnum_u64); }
+
 size_t execute_vm_tests()
 {
-     INVOKE_TEST(test_encdec_i8);
+     INVOKE_TEST(test_encdec_s8 );
+     INVOKE_TEST(test_encdec_u8 );
+     INVOKE_TEST(test_encdec_s16);
+     INVOKE_TEST(test_encdec_u16);
+     INVOKE_TEST(test_encdec_s32);
+     INVOKE_TEST(test_encdec_u32);
+     INVOKE_TEST(test_encdec_s64);
+     INVOKE_TEST(test_encdec_u64);
 
-     if (test_fail_count > 0)
-          fprintf(stderr, "%d TEST%s FAILED.\n",
-                  (int)test_fail_count, ((test_fail_count == 1) ? "" : "S"));
+     if (assert_fail_count > 0)
+          fprintf(stderr, "%d ASSERT%s FAILED.\n",
+                  (int)assert_fail_count, ((assert_fail_count == 1) ? "" : "S"));
      else
           fprintf(stderr, "All tests passed.\n");
 
-     return test_fail_count;
+     return assert_fail_count;
 }
