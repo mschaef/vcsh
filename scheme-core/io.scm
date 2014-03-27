b5c73c3f5c6e055811815580be5a22c4488b008b
Mike Schaeffer <mschaeffer@ksmpartners.com>
2014-03-27 13:25:03 -0400

    Switch to the VM implementations of the input and output port predictes.

--- scheme-core/io.scm
+++ scheme-core/io.scm
@@ -21,22 +21,6 @@
        (cdr it)
        #f))
 
-(define (input-port? port)
-  "Determines if <port> is an input port. Returns the port if it is an
-input port and #f otherwise."
-  (if (and (port? port)
-            (eq? (port-mode port) :input))
-      port
-      #f))
-
-(define (output-port? port)
-  "Determines if <port> is an input port. Returns the port if it is an
-input port and #f otherwise."
-  (if (and (port? port)
-           (eq? (port-mode port) :output))
-      port
-      #f))
-
 (define (port-closed? port)
   "Determines if <port> is closed. Returns the port if it is closed,
 and #f otherwise. If <port> is not a port, throws an error."
