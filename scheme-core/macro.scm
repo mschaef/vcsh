c69a430c0c60a20989a2b68a1d3bea4fd9fbc0f8
Mike Schaeffer <mike@mschaef.com>
2018-09-27 21:10:57 -0400

    Update defmacro to use correct abstractions for processing document strings.

--- scheme-core/macro.scm
+++ scheme-core/macro.scm
@@ -10,22 +10,16 @@
 ;;;; redistribution of this file, and for a DISCLAIMER OF ALL
 ;;;; WARRANTIES.
 
-
 (defmacro (defmacro lambda-list . macro-body) ;; REVISIT: Macros with :optional arguments
   (runtime-check valid-lambda-list? lambda-list)
-  (let ((macro-body macro-body) ; shadowed the argument to give us something settable.
-        (macro-name (car lambda-list))
-        (macro-formals (cdr lambda-list))
-        (macro-documentation #f))
-    (when (and (string? (car macro-body)) (> (length macro-body) 1))
-      (set! macro-documentation (car macro-body))
-      (set! macro-body (cdr macro-body)))
-    `(define ,macro-name
-       (%macrocons (%lambda (,@(if macro-documentation
-                                   `((documentation . ,(normalize-whitespace macro-documentation)))
-                                   ())
-                             (macro-name . ,macro-name)
-                             (macro-formals . ,macro-formals))
-                       (form env)
-                     (dbind ,macro-formals (cdr form)
-                       ,@macro-body))))))
+  (dbind (macro-name . macro-formals) lambda-list
+    (mvbind (doc-string macro-body) (accept-documentable-block macro-body)
+      `(define ,macro-name
+         (%macrocons (%lambda (,@(if doc-string
+                                     `((documentation . ,(normalize-whitespace doc-string)))
+                                     ())
+                               (macro-name . ,macro-name)
+                               (macro-formals . ,macro-formals))
+                         (form env)
+                       (dbind ,macro-formals (cdr form)
+                         ,@macro-body)))))))
